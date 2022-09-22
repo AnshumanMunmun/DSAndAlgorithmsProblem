db1198cf6c7cb7cfeda0339b67abfa7d945cc7a6
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-22 22:30:02 +0530

    [Anshuman] Add: Sorting the names by using Insertions Sort

--- DSAndAlgorithmsProblem/InsertionSort.cs
+++ DSAndAlgorithmsProblem/InsertionSort.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace DSAndAlgorithmsProblem
 {
-    internal class InsertionSort
+    public class InsertionSort
     {
+        public void Sort()
+        {
+            List<string> list = new List<string>() { "kivi", "apple", "pineapple", "banana", "watermelon" };
+            int n = 5, i, j, flag;
+            string val;
+            Console.WriteLine("Initial List is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = list[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val.CompareTo(list[j]) < 0)
+                    {
+                        list[j + 1] = list[j];
+                        j--;
+                        list[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            Console.Write("\nSorted List is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
