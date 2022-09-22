b9d51e239aff2078668cde5e79abd75603edf469
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-22 22:26:38 +0530

    [Anshuman] Add: Sorting the names by using Insertions Sort

--- DSAndAlgorithmsProblem/Program.cs
+++ DSAndAlgorithmsProblem/Program.cs
@@ -6,9 +6,10 @@
         {
             BinarySearch binarySearch = new BinarySearch();
             BubbleSort bubbleSort = new BubbleSort();
+            InsertionSort insertionSort = new InsertionSort();
             while (true)
             {
-                Console.WriteLine("Enter the option : \n1. binarySearch \n2. bubbleSort");
+                Console.WriteLine("Enter the option : \n1. binarySearch \n2. bubbleSort \n3 InsertionSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -17,6 +18,9 @@
                         break;
                     case 2:
                         bubbleSort.Bubble(@"E:\Git\GitRepository\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\numb.txt");
+                        break;
+                    case 3:
+                        insertionSort.Sort();
                         break;
                     default:
                         Console.WriteLine("please choose correct options :");
