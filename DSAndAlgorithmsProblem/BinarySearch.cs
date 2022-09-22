1392628a3a68bbc6880d89d9f4788e7c11dd687d
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-22 21:29:24 +0530

    [Anshuman] Add: Searching a word by using binary search

---
+++ DSAndAlgorithmsProblem/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAndAlgorithmsProblem
+{
+    public class BinarySearch
+    {
+        public void Search(string filePath)
+        {
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(" ");
+            Console.WriteLine("Enter word to Search");
+            string search = Console.ReadLine();
+            foreach (var word in words)
+            {
+                if (search.ToLower().Equals(word.ToLower()))
+                {
+                    Console.WriteLine(word + " Word Found");
+                    return;
+                }
+            }
+            Console.WriteLine("Word Not Found");
+        }
+    }
+}
--- DSAndAlgorithmsProblem/Program.cs
+++ DSAndAlgorithmsProblem/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Data Structure And Alogrithm Problem Statment");
+            BinarySearch binarySearch = new BinarySearch();
+            while (true)
+            {
+                Console.WriteLine("Enter the option : \n1. binarySearch");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        binarySearch.Search(@"E:\Git\GitRepository\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\Words.txt");
+                        break;
+                    default:
+                        Console.WriteLine("please choose correct options :");
+                        break;
+                }
+            }
         }
     }
 }
