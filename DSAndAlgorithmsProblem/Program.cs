51aed5e523c8a927f86900d1d7bc6b7268e8443d
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-22 22:14:23 +0530

    [Anshuman] Add: Sorting the numbers by using Bubble Sort

--- DSAndAlgorithmsProblem/Program.cs
+++ DSAndAlgorithmsProblem/Program.cs
@@ -5,14 +5,18 @@
         static void Main(string[] args)
         {
             BinarySearch binarySearch = new BinarySearch();
+            BubbleSort bubbleSort = new BubbleSort();
             while (true)
             {
-                Console.WriteLine("Enter the option : \n1. binarySearch");
+                Console.WriteLine("Enter the option : \n1. binarySearch \n2. bubbleSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         binarySearch.Search(@"E:\Git\GitRepository\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\Words.txt");
+                        break;
+                    case 2:
+                        bubbleSort.Bubble(@"E:\Git\GitRepository\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\DSAndAlgorithmsProblem\numb.txt");
                         break;
                     default:
                         Console.WriteLine("please choose correct options :");
