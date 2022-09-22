c09a8924a806ec81b9c7f411acbaf25ff457cdbb
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-22 22:37:42 +0530

    [Anshuman] Add: Sorting the numbers by using Merge Sort

--- DSAndAlgorithmsProblem/Program.cs
+++ DSAndAlgorithmsProblem/Program.cs
@@ -7,9 +7,10 @@
             BinarySearch binarySearch = new BinarySearch();
             BubbleSort bubbleSort = new BubbleSort();
             InsertionSort insertionSort = new InsertionSort();
+            MergeSort mergeSort = new MergeSort();
             while (true)
             {
-                Console.WriteLine("Enter the option : \n1. binarySearch \n2. bubbleSort \n3 InsertionSort");
+                Console.WriteLine("Enter the option : \n1. binarySearch \n2. bubbleSort \n3. InsertionSort \n4. MergeSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -21,6 +22,23 @@
                         break;
                     case 3:
                         insertionSort.Sort();
+                        break;
+                    case 4:
+                        int n = 8;
+                        int[] array = { 49, 3, 53, 21, 27, 75, 50, 41 };
+                        Console.WriteLine("Merge Sort");
+                        Console.Write("Initial array is: ");
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }
+                        mergeSort.Sort(array, 0, n - 1);
+                        Console.Write("\nSorted Array is: ");
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }
+                        Console.WriteLine();
                         break;
                     default:
                         Console.WriteLine("please choose correct options :");
